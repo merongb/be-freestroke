d7d21f1a3afb860cc077d4dff7432331b687380a
Millie Ellis <ellismillie92@gmail.com>
2023-11-06 09:44:18 +0000

    added 10 locations and added water_classification to object - avg_rating isn't here as this will be calculated based upon reviews data

--- db/data/test-data/locations.js
+++ db/data/test-data/locations.js
@@ -8,7 +8,7 @@ module.exports = [
         location_img_url: "https://www.coppermines.co.uk/wp-content/uploads/2021/04/Lake-District-Cottages-Fantastic-Facts-About-the-Lake-District-Blog-Image.jpg",
         body: "Description of the location",
         review_count: 10,
-        water_classification: 'Excellent'
+        water_classification: 'excellent'
     },
     {
         coordinates: [53.4084, 2.9916],
@@ -19,7 +19,7 @@ module.exports = [
         location_img_url: "https://www.example.com/beautiful-beach.jpg",
         body: "A breathtaking beach with golden sands.",
         review_count: 15,
-        water_classification: 'Good'
+        water_classification: 'good'
     },
     {
         coordinates: [51.5074, -0.1278],
@@ -30,7 +30,7 @@ module.exports = [
         location_img_url: "https://www.example.com/city-park.jpg",
         body: "A peaceful park in the heart of the city.",
         review_count: 8,
-        water_classification: 'Sufficient'
+        water_classification: 'sufficient'
     },
     {
         coordinates: [48.8588, 2.2945],
@@ -41,7 +41,7 @@ module.exports = [
         location_img_url: "https://www.example.com/eiffel-tower.jpg",
         body: "Enjoy a stunning view of the Eiffel Tower.",
         review_count: 20,
-        water_classification: 'Excellent'
+        water_classification: 'excellent'
     },
     {
         coordinates: [40.7128, -74.0060],
@@ -52,7 +52,7 @@ module.exports = [
         location_img_url: "https://www.example.com/central-park.jpg",
         body: "A serene pond in the heart of Central Park.",
         review_count: 12,
-        water_classification: 'Good'
+        water_classification: 'good'
     },
     {
         coordinates: [34.0522, -118.2437],
@@ -63,7 +63,7 @@ module.exports = [
         location_img_url: "https://www.example.com/sunset-beach.jpg",
         body: "Witness breathtaking sunsets at this beach.",
         review_count: 18,
-        water_classification: 'Excellent'
+        water_classification: 'excellent'
     },
     {
         coordinates: [41.8781, -87.6298],
@@ -74,7 +74,7 @@ module.exports = [
         location_img_url: "https://www.example.com/lakefront-park.jpg",
         body: "Relax by the lake in a beautiful park.",
         review_count: 9,
-        water_classification: 'Sufficient'
+        water_classification: 'sufficient'
     },
     {
         coordinates: [37.7749, -122.4194],
@@ -85,7 +85,7 @@ module.exports = [
         location_img_url: "https://www.example.com/golden-gate.jpg",
         body: "Marvel at the Golden Gate Bridge from here.",
         review_count: 25,
-        water_classification: 'Good'
+        water_classification: 'good'
     },
     {
         coordinates: [35.6895, 139.6917],
@@ -96,7 +96,7 @@ module.exports = [
         location_img_url: "https://www.example.com/cherry-blossom.jpg",
         body: "Enjoy the beauty of cherry blossoms in spring.",
         review_count: 14,
-        water_classification: 'Excellent'
+        water_classification: 'excellent'
     },
     {
         coordinates: [25.276987, 51.517309],
@@ -107,6 +107,6 @@ module.exports = [
         location_img_url: "https://www.example.com/desert-oasis.jpg",
         body: "Experience tranquility in the desert oasis.",
         review_count: 6,
-        water_classification: 'Sufficient'
+        water_classification: 'sufficient'
     }    
 ]

1f8ad10a81836dcd8e0c088ad266a0a92359c9c6
Meron G <merong@Merons-MacBook-Air.local>
2023-11-06 13:43:09 +0000

    seeding data

--- connection.js
+++ connection.js
@@ -1,33 +1,36 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || 'test';
 dotenv.config({
   path: `${__dirname}/../.env.${ENV}`
-});
-
+  
+})
+console.log(ENV)
 if (!process.env.MONGO_URL) {
   throw new Error('MONGO_URL not set')
 }
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
-});
+})
 
 
-function dropAndCreateTestDatabase(callback) {
-    if (process.env.NODE_ENV === 'test') {
-      mongoose.connection.dropDatabase(() => {
-        console.log('Dropped the test database')
-        mongoose.connection.db.createCollection('test-database', () => {
-          console.log('Created the test database')
-          mongoose.connection.close(() => {
-            callback()
-          })
-        })
+function dropAndCreateTestDatabase() {
+  if (process.env.NODE_ENV === 'test') {
+    return mongoose.connection.dropDatabase()
+      .then(() => {
+        return mongoose.connection.db.createCollection('test-database');
       })
-    }
+      .then(() => {
+      })
   }
+}
+
   dropAndCreateTestDatabase()
 
-module.exports = mongoose.connection;
+  module.exports = {
+    db: mongoose.connection,
+    dropAndCreateTestDatabase,
+  }
+  
---
+++ db/seeds/seed.js
@@ -0,0 +1,50 @@
+const { db } = require('../../connection');
+const mongoose = require('mongoose');
+const locationData = require('../data/test-data/locations')
+const reviewData = require('../data/test-data/reviews')
+
+const LocationModel = mongoose.model('Location', new mongoose.Schema({
+  coordinates: [Number],
+  location_id: Number,
+  created_at: Number,
+  location_name: String,
+  location_area: String,
+  location_img_url: String,
+  body: String,
+  review_count: Number,
+  water_classification: String,
+},
+{ versionKey: false }
+));
+
+const ReviewModel = mongoose.model('Review', new mongoose.Schema({
+  uid: String,
+  review_id: Number,
+  username: String,
+  votes: Number,
+  body: String,
+  created_at: Number,
+  location_id: Number,
+},
+{ versionKey: false }
+))
+
+
+function seedData(locationData, reviewData, LocationModel, ReviewModel) {
+    
+    return Promise.all([
+      LocationModel.collection.drop(),
+      ReviewModel.collection.drop(),
+    ])
+      .then(() => {
+        return Promise.all([
+          LocationModel.insertMany(locationData),
+          ReviewModel.insertMany(reviewData),
+        ])
+      }).catch((err) => {
+        console.log(err);
+      })
+
+  }
+  
+  module.exports = { LocationModel, ReviewModel, seedData }

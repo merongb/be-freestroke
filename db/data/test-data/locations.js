18c7aefeb2c60a6d1eb57db792bca59d1b13b213
Millie Ellis <ellismillie92@gmail.com>
2023-11-06 14:47:14 +0000

    removed location id and updated keys to make it clear some are further APIs

--- db/data/test-data/locations.js
+++ db/data/test-data/locations.js
@@ -1,112 +1,92 @@
 module.exports = [
     {
         coordinates : [53.4084,  2.9916],
-        location_id: 1,
         created_at: 1594329060000,
         location_name: "Test lake spot",
         location_area: "Merseyside",
-        location_img_url: "https://www.coppermines.co.uk/wp-content/uploads/2021/04/Lake-District-Cottages-Fantastic-Facts-About-the-Lake-District-Blog-Image.jpg",
-        body: "Description of the location",
-        review_count: 10,
-        water_classification: 'excellent'
+        water_classification: 'excellent',
+        img_and_body_api: "http://environment.data.gov.uk/data/bathing-water-profile/ukc2102-03600/2023:1",
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/03600/date/20230924/time/121500/recordDate/20230924'
     },
     {
         coordinates: [53.4084, 2.9916],
-        location_id: 2,
         created_at: 1594329061000,
         location_name: "Beautiful Beach",
         location_area: "Coastal Paradise",
-        location_img_url: "https://www.example.com/beautiful-beach.jpg",
-        body: "A breathtaking beach with golden sands.",
-        review_count: 15,
-        water_classification: 'good'
+        water_classification: 'good',
+        img_and_body_api: 'http://environment.data.gov.uk/data/bathing-water-profile/ukh3311-11300/2023:1',
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/11300/date/20230913/time/112000/recordDate/20230913'
     },
     {
         coordinates: [51.5074, -0.1278],
-        location_id: 3,
         created_at: 1594329062000,
         location_name: "City Park",
         location_area: "Urban Oasis",
-        location_img_url: "https://www.example.com/city-park.jpg",
-        body: "A peaceful park in the heart of the city.",
-        review_count: 8,
-        water_classification: 'sufficient'
+        water_classification: 'sufficient',
+        img_and_body_api: 'http://environment.data.gov.uk/data/bathing-water-profile/ukh3311-11350/2023:1',
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/11350/date/20230921/time/103800/recordDate/20230921'
     },
     {
         coordinates: [48.8588, 2.2945],
-        location_id: 4,
         created_at: 1594329063000,
         location_name: "Eiffel Tower View",
         location_area: "Paris",
-        location_img_url: "https://www.example.com/eiffel-tower.jpg",
-        body: "Enjoy a stunning view of the Eiffel Tower.",
-        review_count: 20,
-        water_classification: 'excellent'
+        water_classification: 'excellent',
+        img_and_body_api: 'http://environment.data.gov.uk/data/bathing-water-profile/ukh3311-11500/2023:1',
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/11500/date/20230921/time/110800/recordDate/20230921'
     },
     {
         coordinates: [40.7128, -74.0060],
-        location_id: 5,
         created_at: 1594329064000,
         location_name: "Central Park Pond",
         location_area: "New York City",
-        location_img_url: "https://www.example.com/central-park.jpg",
-        body: "A serene pond in the heart of Central Park.",
-        review_count: 12,
-        water_classification: 'good'
+        water_classification: 'good',
+        img_and_body_api: 'http://environment.data.gov.uk/data/bathing-water-profile/ukh3311-11600/2023:1',
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/11600/date/20230921/time/114800/recordDate/20230921'
     },
     {
         coordinates: [34.0522, -118.2437],
-        location_id: 6,
         created_at: 1594329065000,
         location_name: "Sunset Beach",
         location_area: "Los Angeles",
-        location_img_url: "https://www.example.com/sunset-beach.jpg",
-        body: "Witness breathtaking sunsets at this beach.",
-        review_count: 18,
-        water_classification: 'excellent'
+        water_classification: 'excellent',
+        img_and_body_api: 'http://environment.data.gov.uk/data/bathing-water-profile/ukh3311-11650/2023:1',
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/11650/date/20230921/time/112800/recordDate/20230921'
     },
     {
         coordinates: [41.8781, -87.6298],
-        location_id: 7,
         created_at: 1594329066000,
         location_name: "Lakefront Park",
         location_area: "Chicago",
-        location_img_url: "https://www.example.com/lakefront-park.jpg",
-        body: "Relax by the lake in a beautiful park.",
-        review_count: 9,
-        water_classification: 'sufficient'
+        water_classification: 'sufficient',
+        img_and_body_api: 'http://environment.data.gov.uk/data/bathing-water-profile/ukh3311-11700/2023:1',
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/11700/date/20230913/time/140400/recordDate/20230913'
     },
     {
         coordinates: [37.7749, -122.4194],
-        location_id: 8,
         created_at: 1594329067000,
         location_name: "Golden Gate View",
         location_area: "San Francisco",
-        location_img_url: "https://www.example.com/golden-gate.jpg",
-        body: "Marvel at the Golden Gate Bridge from here.",
-        review_count: 25,
-        water_classification: 'good'
+        water_classification: 'good',
+        img_and_body_api: 'http://environment.data.gov.uk/data/bathing-water-profile/ukh3306-11750/2023:1',
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/11750/date/20230921/time/134800/recordDate/20230921'
     },
     {
         coordinates: [35.6895, 139.6917],
-        location_id: 9,
         created_at: 1594329068000,
         location_name: "Cherry Blossom Park",
         location_area: "Tokyo",
-        location_img_url: "https://www.example.com/cherry-blossom.jpg",
-        body: "Enjoy the beauty of cherry blossoms in spring.",
-        review_count: 14,
-        water_classification: 'excellent'
+        water_classification: 'excellent',
+        img_and_body_api: 'http://environment.data.gov.uk/data/bathing-water-profile/ukk1302-11760/2023:1',
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/11760/date/20230925/time/110900/recordDate/20230925'
     },
     {
         coordinates: [25.276987, 51.517309],
-        location_id: 10,
         created_at: 1594329069000,
         location_name: "Desert Oasis",
         location_area: "Qatar",
-        location_img_url: "https://www.example.com/desert-oasis.jpg",
-        body: "Experience tranquility in the desert oasis.",
-        review_count: 6,
-        water_classification: 'sufficient'
+        water_classification: 'sufficient',
+        img_and_body_api: 'http://environment.data.gov.uk/data/bathing-water-profile/ukh3100-11770/2023:1',
+        water_classification_date_api: 'http://environment.data.gov.uk/data/bathing-water-quality/in-season/sample/point/11770/date/20230918/time/132000/recordDate/20230918'
     }    
 ]

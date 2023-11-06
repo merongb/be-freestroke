d4096a50b32dfd71a6e4cfe1d600e5ad6b228483
Millie Ellis <ellismillie92@gmail.com>
2023-11-06 15:12:56 +0000

    updated so that all data is pulled through from 3 different APIs including img, body & water classification date

--- db/data/development-data/api.js
+++ db/data/development-data/api.js
@@ -2,33 +2,37 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-const waterAPI = axios.create({ baseURL: 'https://environment.data.gov.uk/doc/bathing-water?_pageSize=500' });
+const allSwimSpotsAPI = axios.create({ baseURL: 'https://environment.data.gov.uk/doc/bathing-water?_pageSize=500' });
 
-const getCoastalSpots = () => {
-    return waterAPI.get()
-        .then((res) => {
-            const allData = res.data.result.items;
+const getCoastalSpots = async () => {
+    const res = await allSwimSpotsAPI.get();
+    const allSwimSpots = res.data.result.items;
+    const formattedData = [];
+    for (const item of allSwimSpots) {
+        const bodyAndImgAPI = await allSwimSpotsAPI.get(item.latestProfile);
+        const bodyAndImgData = bodyAndImgAPI.data.result.primaryTopic;
+        const waterTestDateAPI = await allSwimSpotsAPI.get(item.latestSampleAssessment);
+        const waterTestDate = waterTestDateAPI.data.result.primaryTopic.sampleDateTime.inXSDDateTime._value
 
-            const formattedData = allData.map(item => ({
-                coordinates: [item.samplingPoint.long, item.samplingPoint.lat],
-                location_name: item.name._value,
-                location_area: item.district[0].name._value,
-                water_classification: item.latestComplianceAssessment
-                    ? item.latestComplianceAssessment.complianceClassification.name._value
-                    : null,
-                created_at: null,
-                water_classification_date_api: item.latestSampleAssessment,
-                img_and_body_api: item.latestProfile,
-            }));
+        const formattedItem = {
+            coordinates: [item.samplingPoint.long, item.samplingPoint.lat],
+            location_name: item.name._value,
+            location_area: item.district[0].name._value,
+            water_classification: item.latestComplianceAssessment
+                ? item.latestComplianceAssessment.complianceClassification.name._value
+                : null,
+            water_classification_date: waterTestDate,
+            location_img_url: bodyAndImgData.webResImage || null,
+            body: bodyAndImgData.bathingWaterDescription._value || null
+        };
+        formattedData.push(formattedItem);
+        const jsonData = JSON.stringify(formattedData, null, 2);
+        const filePath = path.join(process.cwd(), 'db/data/development-data/swim_api_locations.js');
 
-            const jsonData = JSON.stringify(formattedData, null, 2);
-            const filePath = path.join(process.cwd(), 'db/data/development-data/swim_api_locations.js');
-
-            fs.writeFileSync(filePath, jsonData);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
-};
+        fs.writeFileSync(filePath, jsonData);
+    }
+    return formattedData
+}
 
 getCoastalSpots();
+    

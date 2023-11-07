b9609e48bdec8dd30b9d4498c5690c50c460c2d5
Millie Ellis <ellismillie92@gmail.com>
2023-11-07 13:02:50 +0000

    added POST review and updated reviews test-data

--- app/error-handler.js
+++ app/error-handler.js
@@ -1,7 +1,9 @@
-// Error Handling
 exports.handleMongoErrors = (err, req, res, next) => {
 	if (err.name === 'CastError' && err.kind === 'Number') {
 		return res.status(400).send({ message: "Bad Request" });
+	}
+	if (err.errors.location_id.path === 'location_id' && err.errors.location_id.reason.code === 'ERR_ASSERTION') {
+		return res.status(400).send({ message: "Invalid ID" });
 	}
 	next(err)
 };

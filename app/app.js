8103fc4f6d65c47d092756beb9df33b6f17d4e99
Saad Qureshi <saad-qureshi2001@outlook.com>
2023-11-07 10:58:16 +0000

    2/3 Tests Passing

--- app/app.js
+++ app/app.js
@@ -9,35 +9,32 @@ const {
     getLocationById
 } = require('../app/controllers/locations_controller');
 
+const {
+	getReviewsByLocationId,
+} = require("../app/controllers/reviews_controller");
 
 
 
-// const { handleMongoErrors, handleErrors } = require("./db/error-handler");
+
+
+const { handleMongoErrors, handleErrors } = require("./error-handler");
 
 
 // app.get("/api", getAllEndpoints);
 app.get("/api/locations", getAllLocations);
 app.get("/api/locations/:location_id", getLocationById);
-// app.get("/api/location/:location_id/reviews", getReviewsById )
+app.get("/api/location/:location_id/reviews", getReviewsByLocationId )
 // app.post("/api/location/:location_id/reviews", postReview)
 // app.patch("/api/locations/:location_id", patchLocationById)
 // app.delete("/api/location/:location_id/reviews", removeReview)
 
-// app.use(handleMongoErrors)
-// app.use(handleErrors)
-app.use((err, req, res, next) => {
-    if (err.status){
-        res.status(err.status).send({message: err.message})
-    } else {
-        console.log(err);
-        res.status(500).send({message: "Internal Server Error"})
-    }
-})
+app.use(handleMongoErrors)
+app.use(handleErrors)
 
 
 app.all("*", (req, res) => {
-    res.status(404).send({ msg: "Not found" });
+    res.status(404).send({ message: "Not found" });
 });
 
-
 module.exports = app;
+
---
+++ app/error-handler.js
@@ -0,0 +1,14 @@
+// Error Handling
+exports.handleMongoErrors = (err, req, res, next) => {
+	if (err.name === 'CastError' && err.kind === 'Number') {
+		return res.status(400).send({ message: "Bad Request" });
+	  }
+};
+
+exports.handleErrors = (err, req, res, next) => {
+	if (err.status) {
+		res.status(err.status).send({ message: err.message });
+	} else {
+		next(err);
+	}
+};
